2f225fd1b85b9beac4912c91377562875377fdf8
amin2ace <amin.vmap3240@gmail.com>
2025-04-14 19:53:02 +0330

    Install microservices package and define rabbitMq options

--- libs/common/src/rmq/rmq.service.ts
+++ libs/common/src/rmq/rmq.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { RmqOptions, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class RmqService {
   constructor(protected readonly configService: ConfigService) {}
+
+  async getOptions(queue: string, noAck = false): Promise<RmqOptions> {
+    return {
+      transport: Transport.RMQ,
+      options: {
+        urls: [this.configService.getOrThrow<string>('RABBITMQ_URL')],
+        queue: this.configService.getOrThrow<string>(`RABBITMQ_${queue}_QUEUE`),
+        noAck,
+      },
+    };
+  }
 }
