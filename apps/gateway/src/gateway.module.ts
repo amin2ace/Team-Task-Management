4768af3f1709c1a54a2e9ee65a1a9b7d7c7c4ee3
Amin Vahdani <amin.vahdani@payever.org>
2025-04-29 21:30:46 +0330

    Define gateway module

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
-import { join } from 'path';
+import { ClientsModule } from '@nestjs/microservices';
+import { RmqService } from './rmq.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmOptions } from './typeorm.options';
+import path from 'path';
+// import { RmqModule } from '@app/common';
 
 @Module({
   imports: [
+    ClientsModule.registerAsync({
+      isGlobal: true,
+      clients: [
+        {
+          inject: [ConfigService],
+          name: 'GatewayService.name',
+          useClass: RmqService,
+        },
+      ],
+    }),
+
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'apps/gateway/.env',
+      envFilePath: path.join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
       validationSchema: Joi.object({
         // App Variables
         PORT: Joi.string().required(),
@@ -17,6 +33,10 @@ import { join } from 'path';
         // Swagger Variables
         SWAGGER_PATH: Joi.string().required(),
         SWAGGER_SERVER_URL: Joi.string().required(),
+
+        // RabbitMq Variables
+        RABBITMQ_URL: Joi.string().required(),
+        RABBITMQ_SERVICE_QUEUE: Joi.string().required(),
       }),
     }),
   ],
