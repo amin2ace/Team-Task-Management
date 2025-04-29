76772654499023eda2aa2c66ecb972be97d5a41f
Amin Vahdani <amin.vahdani@payever.org>
2025-04-29 23:21:20 +0330

    change create team pattern name

--- apps/gateway/src/gateway.service.ts
+++ apps/gateway/src/gateway.service.ts
@@ -2,17 +2,19 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CreateTeamRequestDto, CreateTeamResponseDto } from './dto';
 import { ClientProxy } from '@nestjs/microservices';
+import { RmqTeamService } from './rmq/rmq-team.service';
 
 @Injectable()
 export class GatewayService {
   constructor(
     protected readonly configServcie: ConfigService,
-    @Inject(GatewayService.name) private readonly client: ClientProxy,
+    @Inject(RmqTeamService.name) private readonly client: ClientProxy,
   ) {}
 
   protected logger = new Logger(GatewayService.name);
   async createNewTeam(teamData: CreateTeamRequestDto) {
-    await this.client.emit('createTeam.pattern', teamData);
+    await this.client.emit('team.create', teamData);
+
     this.logger.debug(`${teamData} was sent`);
     return teamData;
   }
