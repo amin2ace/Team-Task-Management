80b0f0877b22e75a3743517cc7be34edc24fde57
Amin Vahdani <amin.vahdani@payever.org>
2025-04-29 21:33:26 +0330

    create route for team creation

--- apps/gateway/src/gateway.controller.ts
+++ apps/gateway/src/gateway.controller.ts
@@ -1,15 +1,15 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { GatewayService } from './gateway.service';
 import { CreateTeamRequestDto, CreateTeamResponseDto } from './dto';
+import { Serialize } from './interceptor/serialize.interceptor';
 
-@Controller('/')
+@Controller()
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
 
-  @Post('/team/create')
-  async createNewTeam(
-    @Body() teamData: CreateTeamRequestDto,
-  ): Promise<CreateTeamResponseDto> {
+  @Serialize(CreateTeamResponseDto)
+  @Post('/create_team')
+  async createNewTeam(@Body() teamData: CreateTeamRequestDto) {
     return this.gatewayService.createNewTeam(teamData);
   }
 }
