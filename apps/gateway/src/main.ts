0ec57e90e9ef729f196270edd531e72700740fb1
Amin Vahdani <amin.vahdani@payever.org>
2025-04-29 23:21:47 +0330

    renaming and refactoring

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,39 +3,40 @@ import { GatewayModule } from './gateway.module';
 import { ConfigService } from '@nestjs/config';
 import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestMicroserviceOptions } from '@nestjs/common/interfaces/microservices/nest-microservice-options.interface';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 // import { RmqService } from '@app/common';
 
-const configService = new ConfigService();
+const config = new ConfigService();
 
 const logger = new Logger('GateWay Main', { timestamp: true });
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
+  const app = await NestFactory.create(GatewayModule, { bufferLogs: false });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Removes any properties from the incoming request that are not defined in DTO.
     }),
   );
 
-  const port: string = configService.getOrThrow<string>('PORT');
+  const port: string = config.getOrThrow<string>('PORT');
 
-  await swaggerApi(app, port);
   app.startAllMicroservices();
   app.useLogger(new Logger());
 
+  await swaggerApi(app, port);
   await app.listen(port, () => {
     logger.debug(`HTTP Gateway started at port ${port}`);
   });
 }
 
 async function swaggerApi(app: INestApplication, port: string) {
-  const swaggerPath = configService.getOrThrow<string>('SWAGGER_PATH');
-  const swaggerUrl = configService.getOrThrow<string>('SWAGGER_SERVER_URL');
+  const swaggerPath = config.getOrThrow<string>('SWAGGER_PATH');
+  const swaggerUrl = config.getOrThrow<string>('SWAGGER_SERVER_URL');
 
   const document = new DocumentBuilder()
     .setTitle('HTTP Gateway')
     .setVersion('0.1')
-    // .addServer(`${swaggerUrl}:${port}/${swaggerPath}`)
     .build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, document);
