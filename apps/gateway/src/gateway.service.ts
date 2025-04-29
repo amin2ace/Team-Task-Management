dc2a26559801f80fbc04cfb6e4c7f85e16b3c435
Amin Vahdani <amin.vahdani@payever.org>
2025-04-29 21:32:20 +0330

    Create service for team route handlers

--- apps/gateway/src/gateway.service.ts
+++ apps/gateway/src/gateway.service.ts
@@ -1,14 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CreateTeamRequestDto, CreateTeamResponseDto } from './dto';
+import { ClientProxy } from '@nestjs/microservices';
 
+import { firstValueFrom } from 'rxjs';
 @Injectable()
 export class GatewayService {
-  constructor(protected readonly configServcie: ConfigService) {}
+  constructor(
+    protected readonly configServcie: ConfigService,
+    @Inject('GatewayService.name') private readonly client: ClientProxy,
+  ) {}
 
-  async createNewTeam(
-    teamData: CreateTeamRequestDto,
-  ): Promise<CreateTeamResponseDto> {
-    return '';
+  protected logger = new Logger(GatewayService.name);
+  async createNewTeam(teamData: CreateTeamRequestDto) {
+    await this.client.emit('createTeam.pattern', teamData);
+    this.logger.debug(`${teamData} was sent`);
+    return teamData;
   }
 }
